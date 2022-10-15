3bdcc25e53477078a4046fbdcb730378a0a93eda
Rodrigo Santos <rodrigosantos.git@gmail.com>
2022-10-15 15:52:05 -0300

    Create menu component

---
+++ src/pages/Menu/index.tsx
@@ -0,0 +1,12 @@
+import styles from './Menu.module.scss';
+import { ReactComponent as Logo } from 'assets/logo.svg';
+
+export default function Menu() {
+  return (
+    <main>
+      <nav className={styles.menu}>
+        <Logo />
+      </nav>
+    </main>
+  );
+}

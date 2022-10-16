995ab57aae55227839612adfc62ce0597ba61c03
Rodrigo Santos <rodrigosantos.git@gmail.com>
2022-10-16 16:19:15 -0300

    Add icon, input and styles

---
+++ src/pages/Menu/SearchEngine/index.tsx
@@ -0,0 +1,22 @@
+import React from 'react';
+import styles from './SearchEngine.module.scss';
+import { CgSearch } from 'react-icons/cg';
+
+interface Props {
+  search: string;
+  setSearch: React.Dispatch<React.SetStateAction<string>>;
+}
+
+export default function SearchEngine({ search, setSearch }: Props) {
+  return (
+    <>
+      <div className={styles.searchEngine}>
+        <input
+          value={search}
+          onChange={(event) => setSearch(event.target.value)}
+        />
+        <CgSearch size={20} color="#4C4D5E" />
+      </div>
+    </>
+  );
+}
--- src/pages/Menu/index.tsx
+++ src/pages/Menu/index.tsx
@@ -1,7 +1,11 @@
 import styles from './Menu.module.scss';
 import { ReactComponent as Logo } from 'assets/logo.svg';
+import SearchEngine from './SearchEngine';
+import { useState } from 'react';
 
 export default function Menu() {
+  const [search, setSearch] = useState('');
+
   return (
     <main>
       <nav className={styles.menu}>
@@ -10,6 +14,10 @@ export default function Menu() {
       <header className={styles.header}>
         <div className={styles.header__text}>A casa do código e da massa</div>
       </header>
+      <section className={styles.menu}>
+        <h3 className={styles.menu__title}>Cardápio</h3>
+        <SearchEngine search={search} setSearch={setSearch} />
+      </section>
     </main>
   );
 }

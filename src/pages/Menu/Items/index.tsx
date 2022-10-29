f02a7182db8825eba00ad8ef2e6064dad6729f82
Rodrigo Santos <rodrigosantos.git@gmail.com>
2022-10-29 11:44:47 -0300

    Add search

--- src/pages/Menu/Items/index.tsx
+++ src/pages/Menu/Items/index.tsx
@@ -1,11 +1,44 @@
 import menu from './items.json';
 import Item from './Item';
 import styles from './Items.module.scss';
+import { useEffect, useState } from 'react';
 
-export default function Items() {
+interface Props {
+  search: string;
+  filter: number | null;
+  sort: string;
+}
+
+export default function Items(props: Props) {
+  const [list, setList] = useState(menu);
+  const { search, filter } = props;
+
+  function testSearch(title: string) {
+    // 'i' -> case insensitive
+    const regex = new RegExp(search, 'i');
+
+    return regex.test(title);
+  }
+
+  function testFilter(id: number) {
+    if (filter !== null) {
+      return filter === id;
+    }
+
+    return true;
+  }
+
+  useEffect(() => {
+    const newList = menu.filter(
+      (item) => testSearch(item.title) && testFilter(item.category.id)
+    );
+
+    setList(newList);
+  }, [search, filter]);
+
   return (
     <div className={styles.items}>
-      {menu.map((item) => (
+      {list.map((item) => (
         <Item key={item.id} {...item} />
       ))}
     </div>
--- src/pages/Menu/index.tsx
+++ src/pages/Menu/index.tsx
@@ -26,7 +26,7 @@ export default function Menu() {
           <Filters filter={filter} setFilter={setFilter} />
           <Order order={order} setOrder={setOrder} />
         </div>
-        <Items />
+        <Items search={search} filter={filter} sort={order} />
       </section>
     </main>
   );

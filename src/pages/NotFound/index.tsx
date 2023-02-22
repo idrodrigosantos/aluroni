c1e85d07b487f56d0b05eecfd3a5fb7abbe20aaa
Rodrigo Santos <rodrigosantos.git@gmail.com>
2023-02-22 15:07:19 -0300

    Create page not found

---
+++ src/pages/NotFound/index.tsx
@@ -0,0 +1,24 @@
+import styles from './NotFound.module.scss';
+import stylesTheme from 'styles/Theme.module.scss';
+import { ReactComponent as NotFoundImage } from 'assets/not-found.svg';
+import classNames from 'classnames';
+import { useNavigate } from 'react-router-dom';
+
+export default function NotFound() {
+  const navigate = useNavigate();
+
+  return (
+    <div
+      className={classNames({
+        [styles.container]: true,
+        [stylesTheme.container]: true,
+      })}
+    >
+      <div className={styles.back}>
+        <button onClick={() => navigate(-1)}>{'< Voltar'}</button>
+      </div>
+
+      <NotFoundImage />
+    </div>
+  );
+}
--- src/routes.js
+++ src/routes.js
@@ -4,11 +4,12 @@ import HeaderMenu from 'components/HeaderMenu';
 import Home from 'pages/Home';
 import Menu from 'pages/Menu';
 import About from 'pages/About';
+import NotFound from 'pages/NotFound';
 import Footer from 'components/Footer';
 
 export default function AppRouter() {
   return (
-    <main>
+    <main className="container">
       <Router>
         <NavigationMenu />
 
@@ -20,6 +21,7 @@ export default function AppRouter() {
 
             <Route path="sobre" element={<About />} />
           </Route>
+          <Route path="*" element={<NotFound />} />
         </Routes>
 
         <Footer />
